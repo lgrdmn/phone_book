32733f0d3e0537de7ce6a70780112f7c3a33e417
lgrdmn <mv_tobletka@mail.ru>
2020-10-11 14:40:04 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,76 @@
-action = input('Введите A, если хотите добавить абонента \n'
-               'Введите D, если хотите удалить абонента \n'
-               'Введите F, если хотите найти абонента \n')
-if action == 'A':
-      add_sub = input('Введите данные абонента в формате: имя, номер, адрес \n')
-      phonebook = open('pb.txt', 'a')
-      phonebook.write(add_sub + '\n')
-      phonebook.close()
-      print('Абонент добавлен в телефонный справочник ')
-elif action == 'D':
-      del_sub = input('Введите имя, номер или адрес абонента которого хотите УДАЛИТЬ \n')
-      file = open('pb.txt', 'r+')
-      file = file.readlines()
-      for i in range(0, len(file)):
-          if del_sub in file[i]:
-              del file[i]
-              file = ''.join(file)
-              print('Абонент удалён из телефонного справочника')
-              phonebook = open('pb.txt', 'w')
-              phonebook.write(file)
-              phonebook.close()
-elif action == 'F':
-      find_sub = input('Введите имя, номер или адрес абонента которого хотите НАЙТИ \n')
-      file = open('pb.txt', 'r')
-      file = file.readlines()
-      for i in range(0, len(file)):
-          if find_sub in file[i]: print('Абонент есть в телефонном справочнике:', file[i])
-else:
-      print('Вы ввели неверную команду ')
+import json
 
+
+def get_phonebook():
+    """Считывание телефонного справочника"""
+    with open('phone_book.json', 'r') as file:
+        return json.load(file)
+
+
+def write_json(subs):
+    """Запись абонента в телефонный справочник"""
+    with open('phone_book.json', 'w') as file:
+        json.dump(subs, file)
+
+
+def add_subscriber():
+    """Добавление абонента в телефонный справочник"""
+    subscriber = dict()
+    subscriber["first_name"] = input("Введите имя абонента: ")
+    subscriber["last_name"] = input("Введите фамилию абонента: ")
+    subscriber["phone_number"] = input("Введите телефонный номер абонента: ")
+    subscriber["street_name"] = input("Введите улицу абонента: ")
+    subscriber["house_number"] = input("Введите номер дома абонента: ")
+    phonebook = get_phonebook()
+    for sub in phonebook:
+        if subscriber["phone_number"] == sub["phone_number"]:
+            print('Абонент c таким номером телефона уже есть в телефонном справочнике:', sub["first_name"],
+                  sub["last_name"], sub["phone_number"], sub["street_name"], sub["house_number"])
+            return
+    phonebook.append(subscriber)
+    write_json(phonebook)
+    print('Абонент:', subscriber["first_name"], subscriber["last_name"], subscriber["phone_number"],
+          subscriber["street_name"], subscriber["house_number"], 'добавлен в телефонный справочник')
+
+
+def find_subscriber():
+    """Поиск абонента в телефонном справочнике по номеру телефона"""
+    find_number = input("Введите телефонный номер абонента которого хотите НАЙТИ: ")
+    phonebook = get_phonebook()
+    for sub in phonebook:
+        if find_number == sub["phone_number"]:
+            print('Абонент есть в телефонном справочнике:', sub["first_name"], sub["last_name"],
+                  sub["phone_number"], sub["street_name"], sub["house_number"])
+            return
+    print('Абонент отсутствует в телефонном справочнике.')
+
+
+def del_subscriber():
+    """Удаление абонента из телефонного справочника по номеру телефона"""
+    del_number = input("Введите телефонный номер абонента которого хотите УДАЛИТЬ: ")
+    phonebook = get_phonebook()
+    for sub in phonebook:
+        if del_number == sub["phone_number"]:
+            print('Абонент:', sub["first_name"], sub["last_name"], sub["phone_number"],
+                  sub["street_name"], sub["house_number"], 'удалён из телефонного справочника')
+            phonebook.remove(sub)
+            write_json(phonebook)
+            return
+    print('Абонент отсутствует в телефонном справочнике.')
+
+
+while True:
+    command = input('Введите A, если хотите добавить абонента \n'
+                    'Введите D, если хотите удалить абонента \n'
+                    'Введите F, если хотите найти абонента \n'
+                    'Введите E, если хотите выйти \n')
+    if command == 'A' or command == 'a':
+        add_subscriber()
+    elif command == 'D' or command == 'd':
+        del_subscriber()
+    elif command == 'F' or command == 'f':
+        find_subscriber()
+    elif command == 'E' or command == 'e':
+        break
+    else:
+        print('Вы ввели неверную команду.')
